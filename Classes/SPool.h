37b649efef32a89ea3c76ad34292a2f1faaf0e96
Huy <duchuykun@gmail.com>
2015-03-31 14:04:23 +0700

    Fix - Minor bugs.

--- Classes/SPool.h
+++ Classes/SPool.h
@@ -6,14 +6,14 @@
 //  Copyright (c) 2015 Katana. All rights reserved.
 //
 
-typedef NS_ENUM(NSInteger, SPoolEvent) {
-  SPoolEventOnInitModel,
-  SPoolEventOnAddModel,
-  SPoolEventOnRemoveModel,
-  SPoolEventOnChange
-};
-
 #import <Foundation/Foundation.h>
+
+typedef NS_ENUM(NSInteger, SPoolEvent) {
+    SPoolEventOnInitModel,
+    SPoolEventOnAddModel,
+    SPoolEventOnRemoveModel,
+    SPoolEventOnChange
+};
 
 @interface SPool : NSObject
 
