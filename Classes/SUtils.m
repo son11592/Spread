1d7e5e44b557ac98c2ab2578d71c6920e84db507
Huy <duchuykun@gmail.com>
2015-04-15 15:41:57 +0700

    Fix - Request with parameters.

--- Classes/SUtils.m
+++ Classes/SUtils.m
@@ -38,30 +38,54 @@
     return sharedInstance;
 }
 
++ (NSString *)getGETParametersString:(NSDictionary *)dictionary {
+    
+    NSString *stringParameters = @"";
+    if (dictionary) {
+        NSArray *allKey = [dictionary allKeys];
+        for (NSString *key in allKey) {
+            if ([stringParameters isEqualToString:@""]) {
+                stringParameters = [stringParameters stringByAppendingString:[NSString stringWithFormat:@"%@=%@",
+                                                                              key, [dictionary valueForKey:key]]];
+            } else {
+                stringParameters = [stringParameters stringByAppendingString:[NSString stringWithFormat:@"&%@=%@",
+                                                                              key, [dictionary valueForKey:key]]];
+            }
+        }
+    }
+    return stringParameters;
+}
+
++ (NSData *)getPOSTParameters:(NSDictionary *)dictionary {
+    
+    NSData *postdata = [NSJSONSerialization dataWithJSONObject:dictionary
+                                                       options:NSJSONWritingPrettyPrinted
+                                                         error:NULL];
+    return postdata;
+}
+
 + (void)request:(NSString *)url
          method:(NSString *)method
      parameters:(NSDictionary *)parameters
 completionHandler:(void(^)(id, NSError *))completion {
     
-    // Create url with parameteres.
-    NSString *stringParameters = @"";
-    if (parameters) {
-        NSArray *allKey = [parameters allKeys];
-        for (NSString *key in allKey) {
-            if ([stringParameters isEqualToString:@""]) {
-                stringParameters = [stringParameters stringByAppendingString:[NSString stringWithFormat:@"%@=%@", key, [parameters valueForKey:key]]];
-            } else {
-                stringParameters = [stringParameters stringByAppendingString:[NSString stringWithFormat:@"&%@=%@", key, [parameters valueForKey:key]]];
-            }
-        }
-    }
-    
     // Make request.
-    NSURL *requestUrl = [NSURL URLWithString:url];
+    NSURL *requestUrl = nil;
+    if ([method isEqualToString:@"GET"] && parameters) {
+        requestUrl = [NSURL URLWithString:[NSString stringWithFormat:@"%@?%@", url, [self getGETParametersString:parameters]]];
+    } else {
+        requestUrl = [NSURL URLWithString:url];
+    }
     NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL:requestUrl];
     NSString *charset = (__bridge NSString *)CFStringConvertEncodingToIANACharSetName(CFStringConvertNSStringEncodingToEncoding(NSUTF8StringEncoding));
-    [request setValue:[NSString stringWithFormat:@"application/json; charset=%@", charset] forHTTPHeaderField:@"Content-Type"];
+    [request setValue:[NSString stringWithFormat:@"application/json; charset=%@", charset]
+   forHTTPHeaderField:@"Content-Type"];
     [request setHTTPMethod:method];
+    if (parameters && ([method isEqualToString:@"POST"]
+                       || [method isEqualToString:@"PUT"]
+                       || [method isEqualToString:@"DELETE"])) {
+        [request setHTTPBody:[self getPOSTParameters:parameters]];
+    }
     [request setTimeoutInterval:API_TIMEOUT_INTERVAL];
     [NSURLConnection sendAsynchronousRequest:request queue:[[self sharedInstance] operationQueue]
                            completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
